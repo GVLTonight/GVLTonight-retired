2959854a74c32343bdf03f1648bf0925afd2c2e1
Kevin Bost <kevbost@gmail.com>
2016-11-24 18:03:14 -0500

    unified dates throughout current venues

--- scrubber/venues/cabin-floor.js
+++ scrubber/venues/cabin-floor.js
@@ -1,5 +1,6 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
+const moment = require('moment');
 
 let url = 'https://www.facebook.com/Cabin-Floor-Records-126363234062075/events/?ref=page_internal';
 let local_path = './scrubber/casper/html/cabin-floor.html';
@@ -7,7 +8,7 @@ let shows = [];
 
 cabin_floor = function(done) {
     let $ = cheerio.load(fs.readFileSync(local_path));
-    $('._51mx').each(function(i, elem) {
+    $('._4-u3:nth-of-type(1)').find('._51mx').each(function(i, elem) {
         let $$ = cheerio.load(elem);
         let raw_date = $$('._4dme').text();
         let raw_time = $$('.fsm > span').text();
@@ -19,7 +20,7 @@ cabin_floor = function(done) {
             title: $$('._4dmk').text(),
             url: $$('._4dmk').attr('href'),
             time: time,
-            date: date
+            date: normalizeDate(date, time)
         };
         shows.push(show);
         // console.log(shows);
@@ -29,16 +30,17 @@ cabin_floor = function(done) {
     }
 
     done(null, shows);
+    // console.log(shows)
 };
 
-normalizeDate = function(date) {
-    date = date.split('/');
-    let month = date.shift();
-    let day = date.shift();
-    date.push(month);
-    date.push(day);
-    return date.join('-');
-};
+function normalizeDate(month_date, day) {
+    let month = month_date.substring(0, 3);
+    let dayof = month_date.substring(3, 1000);
+
+    let rebuilt_date = moment().format('YYYY') + '-' + month + '-' + dayof;
+    let momentDate = moment(rebuilt_date, 'YYYY-MMM-D').format('YYYY-MM-DD');
+    return momentDate;
+}
 
 module.exports = cabin_floor;
 
--- scrubber/venues/peace-center.js
+++ scrubber/venues/peace-center.js
@@ -1,25 +1,28 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
+const moment = require('moment');
 
 const url = 'http://www.peacecenter.org/events';
 const local_path = './scrubber/casper/html/peace-center.html';
 let shows = [];
+let span;
+let date;
 
 peace_center = function(done) {
     let $ = cheerio.load(fs.readFileSync(local_path));
     $('.entry').each(function(i, elem) {
         let $$ = cheerio.load(elem);
         let raw_date = $$('.date').text();
-        let date = raw_date.replace(new RegExp('t', 'g'), '');
-        // var date = raw_date.split('T')[0];
-        // var time = raw_date.split('T')[1];
+        let cleaned_date = raw_date.replace(new RegExp('t', 'g'), '');
+        normalizeDate(cleaned_date);
+
         let show = {
             venue: 'Peace Center',
             venueUrl: url,
             title: $$('h3').text(),
             url: $$('.more').attr('href'),
-            time: null,
-            date: date
+            date: date,
+            span: span
         };
         shows.push(show);
     });
@@ -28,6 +31,45 @@ peace_center = function(done) {
         console.log('peace center OKAY');
     }
     done(null, shows);
+    // console.log(shows);
 };
+
+normalizeDate = function(input) {
+    if (input.indexOf('-')  > 0) {
+        prebuildDateSpan(input);
+    } else {
+        completeSplit(input);
+    }
+};
+
+function prebuildDateSpan(input) {
+    let split_date = input.split(' - ');
+    let start = completeSplit(split_date[0]);
+    let end = completeSplit(split_date[1]);
+    span = [start, end];
+    // console.log(span);
+    return span;
+}
+
+function momentDiff(start, end) {
+    let startMoment = moment(start);
+    let endMoment = moment(end);
+    let diff = endMoment.diff(startMoment, 'days');
+    console.log(diff);
+}
+
+function completeSplit(input) {
+    let split_date = input.split('.');
+    let month = split_date[0];
+    let day = split_date[1];
+    let year = split_date[2];
+    let rebuilt_date = ['20' + year, month, day];
+    let returnValue = rebuilt_date.join('-');
+    date = moment(returnValue, 'YYYY-M-D').format('YYYY-MM-DD');
+    // console.log(momentDate);
+    return date;
+}
+
+// peace_center();
 
 module.exports = peace_center;
