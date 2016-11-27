0a6bb9bf6952688756e77e9ba7d9748ec9dcddd3
Kevin Bost <kevbost@gmail.com>
2016-11-27 15:20:18 -0500

    made new dumb temp site @ ghpages

---
+++ app/scripts/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+});
+
+// let sizer = function(){
+//     let height = 0;
+//     $('h1').each(function(i, elem){
+//         height += $(elem).height();
+//     });
+//     return height;
+// }
+
+// function resizer() {
+//     $('.wrapper').css('height', 'calc(100vh - ' + sizer()) + ' - 40px)';
+// }
+
+// $(window)
+//     .on('resize', function(){
+//         resizer();
+//     })
+//     .on('load', function(){
+//         resizer();
+//     })
