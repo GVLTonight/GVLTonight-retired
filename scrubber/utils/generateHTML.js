3c8b91bec98d733ad2c576456f081edb82f47248
Kevin Bost <kevbost@gmail.com>
2016-11-27 23:37:57 -0500

    fixed $[23:37:52] Using gulpfile ~/Documents/_-Projects/Workspace/GVLTonight/gulpfile.js [23:37:52] Starting 'lint'... [23:37:52] Starting 'styles'... [23:37:53] Starting 'scripts'... [23:37:53] Starting 'images'... [23:37:53] Starting 'fonts'... [23:37:53] Starting 'extras'... [23:37:53] Finished 'fonts' after 51 ms [23:37:54] Finished 'lint' after 1.79 s [23:37:54] Finished 'scripts' after 905 ms [23:37:54] Finished 'images' after 518 ms [23:37:54] Finished 'extras' after 346 ms [23:37:54] Finished 'styles' after 1.61 s [23:37:54] Starting 'html'... [23:37:56] Finished 'html' after 2.62 s [23:37:56] Starting 'build'... [23:37:56] build all files 169.02 kB (gzipped) [23:37:56] Finished 'build' after 78 ms html error

--- scrubber/utils/generateHTML.js
+++ scrubber/utils/generateHTML.js
@@ -27,62 +27,62 @@ module.exports = function(shows, done){
 
 function generateTONIGHT(input){
     var html =
-        '<!doctype html>' +
-        '<html lang="">' +
-        '<head>' +
-        '<meta charset="utf-8">' +
-        '<meta name="description" content="">' +
-        '<meta name="viewport" content="width=device-width, initial-scale=1">' +
-        '<meta name="theme-color" content="#8cc050">' +
-        '<title>GVLTonight</title>' +
-        '<link rel="apple-touch-icon" href="apple-touch-icon.png">' +
-        '<!-- Place favicon.ico in the root directory -->' +
-        '<!-- build:css styles/vendor.css -->' +
-        '<!-- bower:css -->' +
-        '<!-- endbower -->' +
-        '<!-- endbuild -->' +
-        '<!-- build:css styles/main.css -->' +
-        '<link rel="stylesheet" href="styles/main.css">' +
-        '<!-- endbuild -->' +
-        '</head>' +
-        '<body>' +
-        '<!--[if lt IE 10]>' +
-        '<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>' +
-        '<![endif]-->' +
-        '<h1>GVLTonight</h1>' +
-        '<div class="wrapper">' +
-        '<div class="event-list">' +
-        '<pre>' +
-        '<ul>' +
+        '<!doctype html>\n' +
+        '<html lang="">\n' +
+        '<head>\n' +
+        '<meta charset="utf-8">\n' +
+        '<meta name="description" content="">\n' +
+        '<meta name="viewport" content="width=device-width, initial-scale=1">\n' +
+        '<meta name="theme-color" content="#8cc050">\n' +
+        '<title>GVLTonight</title>\n' +
+        '<link rel="apple-touch-icon" href="apple-touch-icon.png">\n' +
+        '<!-- Place favicon.ico in the root directory -->\n' +
+        '<!-- build:css styles/vendor.css -->\n' +
+        '<!-- bower:css -->\n' +
+        '<!-- endbower -->\n' +
+        '<!-- endbuild -->\n' +
+        '<!-- build:css styles/main.css -->\n' +
+        '<link rel="stylesheet" href="styles/main.css">\n' +
+        '<!-- endbuild -->\n' +
+        '</head>\n' +
+        '<body>\n' +
+        '<!--[if lt IE 10]>\n' +
+        '<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>\n' +
+        '<![endif]-->\n' +
+        '<h1>GVLTonight</h1>\n' +
+        '<div class="wrapper">\n' +
+        '<div class="event-list">\n' +
+        '<pre>\n' +
+        '<ul>\n' +
         input +
-        '</ul>' +
-        '</pre>' +
-        '</div>' +
-        '<!-- <img src="images/temp-image.jpg" alt="Oh, you found the alt text! This is the first iteration of GVLTonight.com, a source for everything happening in Greenville SC tonight. That is, everything happening at venues with modern websites. If a venue relies on facebook or old systems to get the word out -- that\'s their bad."> -->' +
-        '</div>' +
-        '<h1 class="link-out">' +
-        '<a href="http://github.com/GVLTonight/GVLTonight">' +
-        'github.com/GVLTonight</a>' +
-        '</h1>' +
-        '<div class="band"></div>' +
-        '<!-- Google Analytics: change UA-XXXXX-X to be your site\'s ID. -->' +
-        '<script>' +
-        '(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=' +
-        'function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;' +
-        'e=o.createElement(i);r=o.getElementsByTagName(i)[0];' +
-        'e.src="https://www.google-analytics.com/analytics.js";' +
-        'r.parentNode.insertBefore(e,r)}(window,document,"script","ga"));' +
-        'ga("create","UA-87165483-1");ga("send","pageview");' +
-        '</script>' +
-        '<!-- build:js scripts/vendor.js -->' +
-        '<!-- bower:js -->' +
-        '<script src="/bower_components/jquery/dist/jquery.js"></script>' +
-        '<!-- endbower -->' +
-        '<!-- endbuild -->' +
-        '<!-- build:js scripts/main.js -->' +
-        '<script src="scripts/main.js"></script>' +
-        '<!-- endbuild -->' +
-        '</body>' +
+        '</ul>\n' +
+        '</pre>\n' +
+        '</div>\n' +
+        '<!-- <img src="images/temp-image.jpg" alt="Oh, you found the alt text! This is the first iteration of GVLTonight.com, a source for everything happening in Greenville SC tonight. That is, everything happening at venues with modern websites. If a venue relies on facebook or old systems to get the word out -- that\'s their bad."> -->\n' +
+        '</div>\n' +
+        '<h1 class="link-out">\n' +
+        '<a href="http://github.com/GVLTonight/GVLTonight">\n' +
+        'github.com/GVLTonight</a>\n' +
+        '</h1>\n' +
+        '<div class="band"></div>\n' +
+        '<!-- Google Analytics: change UA-XXXXX-X to be your site\'s ID. -->\n' +
+        '<script>\n' +
+        '(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=\n' +
+        'function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;\n' +
+        'e=o.createElement(i);r=o.getElementsByTagName(i)[0];\n' +
+        'e.src="https://www.google-analytics.com/analytics.js";\n' +
+        'r.parentNode.insertBefore(e,r)}(window,document,"script","ga"));\n' +
+        'ga("create","UA-87165483-1");ga("send","pageview");\n' +
+        '</script>\n' +
+        '<!-- build:js scripts/vendor.js -->\n' +
+        '<!-- bower:js -->\n' +
+        '<script src="/bower_components/jquery/dist/jquery.js"></script>\n' +
+        '<!-- endbower -->\n' +
+        '<!-- endbuild -->\n' +
+        '<!-- build:js scripts/main.js -->\n' +
+        '<script src="scripts/main.js"></script>\n' +
+        '<!-- endbuild -->\n' +
+        '</body>\n' +
         '</html>';
         // console.log(html)
         return html;
