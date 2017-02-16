f27caa3076688f3db2fa84d97d8b313b5daf3fde
kevbost <kevbost@gmail.com>
2017-02-15 21:26:59 -0500

    nightly update

--- src/dump_DB_to_file.js
+++ src/dump_DB_to_file.js
@@ -60,7 +60,7 @@ function dump_DB_to_file(){
       })
       .on('end', function () {
         console.log('Stream ended');
-        console.log(allData)
+        // console.log(allData)
         fs.truncate("app/db_dump.json", 0, function() {
             fs.writeFile("app/db_dump.json", JSON.stringify(allData), function (err) {
                 if (err) {
