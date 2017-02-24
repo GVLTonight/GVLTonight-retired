5756a3f0c8eacdd667773d8ab175e9f6944f401d
Kevin Bost <kevbost@gmail.com>
2017-02-23 20:18:25 -0500

    deleted scrubber dir

--- scrubber/utils/makePage.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-
-module.exports = function(tonight, thisWeek, done) {
-  var data = tonight
-  data += thisWeek
-fs.writeFile('./app/tempor.html', data, function(){
-    done()
-  })
-}
