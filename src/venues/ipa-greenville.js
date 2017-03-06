d145795cb61d121c1fdf8bbeed184e4439c70fe6
Kevin Bost <kevbost@gmail.com>
2017-03-06 15:26:06 -0500

    fixed null error in ipa

--- src/venues/ipa-greenville.js
+++ src/venues/ipa-greenville.js
@@ -8,35 +8,38 @@ const url = 'https://www.facebook.com/ipagreenville/';
 const local_path = './src/casper/html/ipa-greenville.json';
 let shows = [];
 
-ground_zero = function(done){
+ipa_greenville = function(done){
+
     let res = JSON.parse(fs.readFileSync(local_path));
-    let items = res.events.event;
+    if (res.events != null) {
+        let items = res.events.event;
 
-    for (let i = 0; i < items.length; i++) {
-        let item = items[i];
-        let raw_date = items[i].start_time;
-        let date = moment(raw_date, 'YYYY-MM-DD H:m:ss').format('YYYY-MM-DD');
-        let time = moment(raw_date, 'YYYY-MM-DD H:m:ss').format('h:mm a');
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i];
+            let raw_date = items[i].start_time;
+            let date = moment(raw_date, 'YYYY-MM-DD H:m:ss').format('YYYY-MM-DD');
+            let time = moment(raw_date, 'YYYY-MM-DD H:m:ss').format('h:mm a');
 
-        let show = {
-            venue: 'Independent Public Alehouse',
-            venueUrl: url,
-            title: item.title,
-            description: item.description,
-            url: item.url,
-            time: time,
-            date: date
-        };
-        shows.push(show);
+            let show = {
+                venue: 'Independent Public Alehouse',
+                venueUrl: url,
+                title: item.title,
+                description: item.description,
+                url: item.url,
+                time: time,
+                date: date
+            };
+            shows.push(show);
+        }
+
+        if (shows.length > 0) {
+            console.log('IPA OKAY');
+        }
+
+        done(null, shows);
+        console.log(shows);
     }
-
-    if (shows.length > 0) {
-        console.log('IPA OKAY');
-    }
-
-    done(null, shows);
-    // console.log(shows);
 };
 
-module.exports = ground_zero;
-// ground_zero();
+module.exports = ipa_greenville;
+// ipa_greenville();
