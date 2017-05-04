1facf0d4f7d4c73f207fb6b5325984397f474ef7
kevbost <kevbost@gmail.com>
2017-05-03 23:38:40 -0400

    nightly update

--- src/casper/multiple-urls.js
+++ src/casper/multiple-urls.js
@@ -49,7 +49,7 @@ var venues = [
     {
         url: 'http://www.radioroomgreenville.com',
         title: 'radio-room',
-        selector: '.tour-list'
+        selector: '.tour-venue'
     }
 ];
 
